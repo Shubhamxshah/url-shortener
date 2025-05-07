374c1280febf833a3fa47b68694e061481ce08ec
Shubhamxshah <ritikgujarat@gmail.com>
2025-05-07 19:43:15 +0530

    shortener complete

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,98 @@
-import express from "express";
+import express from 'express';
+import crypto from "crypto";
+import rateLimit from "express-rate-limit";
+import validUrl from "valid-url";
 
 export const app = express();
-app.use(express.json());
+app.use(express.json())
 
-const urls = new Map();
 
-app.post("/api/shorten", (req, res) => {
-  const {url} = req.body;
+const urlDatabase = new Map();
+const stats = new Map();
 
-  const id = generateRandom();
-  urls.set(id, url);
-
-  res.status(201).json({message: `shortened url is ${id}`})
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10
 })
 
-app.get()
+app.use(limiter);
 
-function generateRandom() {
-  const letter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+function generateShortCode() {
+  return crypto.randomBytes(3).toString('hex');
+}
 
-  let result = '';
+// Shorten a URl 
+app.post('/api/shorten', (req, res) => {
+  const {longUrl, expirationDate} = req.body;
 
-  for (let i=0; i<6; i++){
-    result += letter[Math.floor(Math.random() * letter.length)]
+  if (!validUrl.isUri(longUrl)) {
+    res.status(400).json({error: 'Invalod Url'});
+    return;
   }
 
-  return result;
-}
+  const shortCode =  generateShortCode();
+  const expiresAt = expirationDate ? new Date(expirationDate) : null;
+
+  urlDatabase.set(shortCode, {longUrl, expiresAt});
+  stats.set(shortCode, {clicks: 0, createdAt: new Date()});
+
+  const shortUrl = `http://localhost:3001/${shortCode}`;
+  res.json({shortUrl, shortCode });
+})
+
+
+// Retrieve original Url 
+app.get('/api/url/:shortCode', (req, res) => {
+  
+  const {shortCode} = req.params;
+  const urlData = urlDatabase.get(shortCode);
+
+  if (!urlData) {
+    res.status(404).json({error: `Statistics not found`});
+    return;
+  }
+
+  if (urlData.expiresAt && urlData.expiresAt < new Date()) {
+    urlDatabase.delete(shortCode);
+    stats.delete(shortCode);
+    res.status(410).json({error: 'short URL has expired'});
+  }
+
+  res.json({ longUrl: urlData.longUrl });
+});
+
+// Get URL statistics 
+app.get("/api/stats/:shortCode", (req, res) => {
+  const {shortCode} = req.params; 
+  const urlStats = stats.get(shortCode); 
+
+  if (!urlStats) {
+    res.status(404).json({ error: 'Statistics not found'});
+    return;
+  }
+
+  res.json(urlStats);
+})
+
+// Redirect to Original URL 
+app.get("/:shortCode", (req, res) => {
+  const {shortCode} = req.params; 
+  const urlData = urlDatabase.get(shortCode);
+
+  if (!urlData) {
+    res.status(404).send('Short URL not found');
+  }
+
+  if (urlData.expiresAt && urlData.expiresAt < new Date()) {
+    urlDatabase.delete(shortCode); 
+    stats.delete(shortCode);
+    res.status(410).send('short URL has expired');
+    return;
+  }
+
+  const urlStats = stats.get(shortCode);
+  urlStats.clicks += 1;
+  stats.set(shortCode, urlStats);
+
+  res.redirect(301, urlData.longUrl);
+})
